c39134ff95f65ed6d359936b3a34e75793048b5e
M Sufian Khan <sufiancoding@gmail.com>
2025-04-01 12:50:11 +0500

    feat: update reset password page UI/logic

--- app/(private)/menu/reset-password/page.tsx
+++ app/(private)/menu/reset-password/page.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { resetPasswordAction } from "@/app/actions/actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useSearchParams } from "next/navigation";
 
-export default async function ResetPassword(props: {
-  searchParams: Promise<{ message: string }>;
-}) {
-  const searchParams = await props.searchParams;
+export default function ResetPassword() {
+  const searchParams = useSearchParams();
+  const message = searchParams.get("message");
+
   return (
     <div className="max-w-3xl mx-auto py-12 px-4">
       <h1 className="text-3xl font-bold mb-6">Reset Password</h1>
       <p className="text-muted-foreground mb-10">
         Enter your new password below to reset your account password.
       </p>
+
+      {message && (
+        <div className="mb-6 p-4 bg-blue-50 text-blue-600 rounded-md">
+          {message}
+        </div>
+      )}
 
       <form className="max-w-md space-y-6">
         <div className="space-y-2">
